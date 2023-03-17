db56fe55c0fc895c5f42379e03443f44812f190e
缪子文 <751652517@qq.com>
2023-03-17 16:10:14 +0800

    打包之前

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 // 使用ES6导入语法，导入JQuery
 import $ from 'jquery'
 
+// 导入样式
+import './css/index.css'
+import './css/index.less'
+
+// 导入图片，得到图片文件
+import logo from './img/001.jpg'
+// 给img标签的src动态赋值
+$('.box').attr('src', logo)
+
 // 定义JQuery的入口函数
 $(function () {
     // 实现奇偶行变色
-    $('li:odd').css('background-color', 'red')
-    $('li:even').css('background-color', 'yellow')
+    $('li:odd').css('background-color', 'pink')
+    $('li:even').css('background-color', 'skyblue')
 })
+
+// 定义一个装饰器函数
+function info(target) {
+    target.info = 'Person info'
+}
+// 定义一个普通的类
+class Person { }
+console.log(Person)
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,45 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// 1.导入html-webpack-plugin这个插件，得到插件的构造函数
+const HtmlPlugin = require('html-webpack-plugin')
+// 2.new构造函数，创建插件的实例对象
+const htmlPlugin = new HtmlPlugin({
+    // 指定要复制哪个页面
+    template: './src/index.html',
+    // 指定复制出来的文件名和存放路径
+    filename: './index.html'
+})
 
 module.exports = {
-    mode: 'production', // mode用来指定构建模式。可选值有development和production
-    entry: path.join(__dirname, './src/index.js'),  //打包入口文件的路径
+    mode: 'development',
+    entry: './src/index.js',
     output: {
-        path: path.join(__dirname, './dist'),  //输出文件的存放路径
-        filename: 'bundle.js'  //输出文件的名称 
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     devServer: {
-        open: true, //自动打开浏览器
-        compress: true, //开启gzip压缩
-        port: 8080, //服务器端口号
         static: {
-            directory: path.join(__dirname, 'public') // 假设静态文件存放在 public 目录下
-        }
+            directory: path.join(__dirname, 'src'),
+        },
+        port: 8080,
+        open: true
+    },
+    // 3.插件的数组，将来webpack在运行时，会加载并调用这些插件
+    plugins: [htmlPlugin],
+    module: {
+        rules: [
+            // 定义了不同模块对应的loader
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            // 处理.less文件的loader
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            // 处理图片文件的loader
+            // 如果需要调用的loader只有一个，则只传递一个字符串也行，如果有多个loader，则必须指定数组
+            { test: /\.jpg|png|gif$/, use: 'url-loader?limit=22229' },
+            // 使用babel-loader处理高级的JS语法
+            // 早配置babel-loader的时候，程序员只需要把自己的代码进行转换即可；一定要排除node_modules目录中的JS文件
+            // 因为第三方包中的jS兼容性，不需要程序员关心
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }
+        ]
     }
-}
+};
